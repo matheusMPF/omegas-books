a0c76e8ba7d8de86c340ac67f7d8f5761cd8cc95
Matheus <matheu.mpf18@gmail.com>
2023-06-11 13:48:19 -0300

    add as funções de excluir e editar tabela

--- src/Components/Admin/Tables/TypeTables/TableBooks.js
+++ src/Components/Admin/Tables/TypeTables/TableBooks.js
@@ -1,18 +1,38 @@
+import React, { useState } from 'react';
 import FormNewBook from "./FormNewBook/FormNewBook";
-import React from "react";
+import "./FormNewBook/FormNewBook.css"
+
 
 const TableBooks = () => {
 
-    const livros = [
-        {id: "01", title: "Cinderela", gender: "Infantil", 
-        author: "Indefined", cost: "R$ 24,99" },
+    const [books, setBooks] = useState([
+        { id: 1, title: 'Cinderela', genre: 'Infantil', author: ' Charles Perrault', price: 12.99 },
+        { id: 2, title: 'O pequeno príncipe', genre: 'Infantil', author: 'Antoine de Saint-Exupéry', price: 14.99 },
+        { id: 3, title: 'Chapeuzinho vermelho', genre: 'Infantil', author: 'Charles Perrault', price: 12.99 },
+        { id: 4, title: 'A bela e a fera', genre: 'Infantil', author: 'Gabrielle-Suzanne de Villeneuve', price: 12.99 },
+    ]);
 
-        {id: "02", title: "O pequeno príncipe", gender: "Infantil", 
-        author: "Indefined", cost: "R$ 24,99" },
-        
-    ]
+    const [editedBook, setEditedBook] = useState(null);
 
-  return (
+    const handleDelete = (id) => {
+        setBooks(books.filter((book) => book.id !== id));
+    };
+
+    const handleEdit = (book) => {
+        setEditedBook(book);
+    };
+
+    const handleSave = () => {
+        setEditedBook(null);
+    };
+
+    const handleInputChange = (event, field) => {
+        setEditedBook({ ...editedBook, [field]: event.target.value });
+    };
+
+
+
+    return (
         <div className="container">
 
             <div className="container-form">
@@ -21,32 +41,82 @@ const TableBooks = () => {
 
             <div className="table-responsive">
 
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Título</th>
-                                <th>Genêro</th>
-                                <th>Autor</th>
-                                <th>Preço</th>
-                                <th></th>
-                                <th></th>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Título</th>
+                            <th>Genêro</th>
+                            <th>Autor</th>
+                            <th>Preço</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {books.map((book) => (
+                            <tr key={book.id}>
+                                <td>{book.id}</td>
+
+                                <td>{editedBook && editedBook.id === book.id ? (
+                                    <input
+                                        type="text"
+                                        value={editedBook.title}
+                                        onChange={(event) => handleInputChange(event, 'title')}
+                                    />
+                                ) : (
+                                    book.title
+                                )}</td>
+
+                                <td>{editedBook && editedBook.id === book.id ? (
+                                    <input
+                                        type="text"
+                                        value={editedBook.genre}
+                                        onChange={(event) => handleInputChange(event, 'genre')}
+                                    />
+                                ) : (
+                                    book.genre
+                                )}</td>
+
+                                <td>{editedBook && editedBook.id === book.id ? (
+                                    <input
+                                        type="text"
+                                        value={editedBook.author}
+                                        onChange={(event) => handleInputChange(event, 'author')}
+                                    />
+                                ) : (
+                                    book.author
+                                )}</td>
+
+                                <td>{editedBook && editedBook.id === book.id ? (
+                                    <input
+                                        type="text"
+                                        value={editedBook.price}
+                                        onChange={(event) => handleInputChange(event, 'price')}
+                                    />
+                                ) : (
+                                    book.price
+                                )}</td>
+
+                                <td>
+                                    {editedBook && editedBook.id === book.id ? (
+                                        <button className='salvar' onClick={handleSave}>✔️</button>
+                                    ) : (
+                                    
+                                            <button className='editar' onClick={() => handleEdit(book)}>✏️</button>
+                                        
+                                    )}
+                                </td>
+
+                                <td> <button className='excluir' onClick={() => handleDelete(book.id)}>X</button></td>
+
+
+
+
                             </tr>
-                        </thead>
-                        <tbody>
-                            {livros.map((livro) => (
-                                <tr key={livro.id}>
-                                    <td>{livro.id}</td>
-                                    <td>{livro.title}</td>
-                                    <td>{livro.gender}</td>
-                                    <td>{livro.author}</td>
-                                    <td>{livro.cost}</td>
-                                    <td className="excluir">X</td>
-                                    <td className="editar">✏️</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                        ))}
+                    </tbody>
+                </table>
 
             </div>
 
